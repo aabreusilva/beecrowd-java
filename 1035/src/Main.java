16bbb1eda4564e5b4253001e52cb6ff925016558
Alexandre Abreu <contatoalexandre95@gmail.com>
2025-02-24 15:17:13 -0300

    Introducing 4 interger values Then B is greater than c and if d is greater than a, and the sum of c with d is greater than the sum of a and b and if c and d, both are positive and if the variable a for for writing the message accepted values, if not writing values not accepted

---
+++ 1035/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        int d = scanner.nextInt();
+
+        if (b > c && d > a && c > 0 && d > 0 && (c + d) > (a + b) && a % 2 == 0) {
+            System.out.println("Valores aceitos");
+        } else {
+            System.out.println("Valores nao aceitos");
+        }
+        scanner.close();
+    }
+}
